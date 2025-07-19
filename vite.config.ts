f3069996da6b7b61ddd9db003a8eb2d3113a3e14
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-07-18 21:58:32 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/ton-gateway-pix/',
   css: {
     preprocessorOptions: {
       scss: {
